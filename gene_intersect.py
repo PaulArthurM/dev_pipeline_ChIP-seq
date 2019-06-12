c1e6f82da33cda2eae073575756f0918781457ff
Pantheas <paularth.meslin@gmail.com>
2019-06-12 17:26:56 +0200

    Add docstrings to gene_intersect.py's functions.

--- gene_intersect.py
+++ gene_intersect.py
@@ -2,13 +2,16 @@ import sys
 
 
 def genes_in_file(file):
+    """"Take a gene list file and return a python list of all genes."""
     print("Opening {file}".format(file = file))
-    genes = open(file, "r").readlines()
+    lines = open(file, "r").readlines()
+    genes = [line[:-1] for line in lines]
     print("{n} genes in ChIP-seq file".format(n = len(genes)))
     return genes
 
 
 def rna_genes(rna_file):
+    """Take tsv file from DEG analysis and return all genes."""
     print("Opening {file}".format(file = rna_file))
     lines = open(rna_file, "r").readlines()
     genes = [line.split("\t")[1][:-1] for line in lines[1:]]
@@ -17,10 +20,11 @@ def rna_genes(rna_file):
 
 
 def intersect_list(chip, rna):
+    """Take two lists of genes and return commons genes."""
     intersect_list = []
     for cgene in chip:
         for rgene in rna:
-            if (cgene[:-1] == rgene):
+            if (cgene == rgene):
                 if (rgene not in intersect_list):
                     intersect_list.append(rgene)
     print("{n} gene(s) intersected".format(n = len(intersect_list)))
@@ -28,18 +32,23 @@ def intersect_list(chip, rna):
 
 
 def print_genes(list):
+    """Take a list of genes and print them all."""
     for gene in list:
         print("\t",gene)
 
 
 def main(chip_seq_genes=sys.argv[1], rna_seq_genes=sys.argv[2]):
     """Take two lists of genes , and return intersected genes.
-    
+
     chip_seq_genes: text file of ChIP-seq genes (first argument of the script)
     rna_seq_genes: text file of RNA-seq genes (second argument of the script)
-    
+
     """
-    print_genes(intersect_list(genes_in_file(chip_seq_genes), rna_genes(rna_seq_genes)))
+    rna_seq_gene_list = rna_genes(rna_seq_genes)
+    chip_seq_genes_list = genes_in_file(chip_seq_genes)
+    intersected_gene_list = intersect_list(chip_seq_genes_list, rna_seq_gene_list)
+    print_genes(intersected_gene_list)
 
 
-main()
+if __name__ == '__main__':
+    main()
